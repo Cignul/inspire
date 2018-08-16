737f3b60f8320ad566e8a92003af49f05b211e87
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-08-16 14:09:40 -0600

    linking everything up and adding controllers to main

--- app/components/image/image-controller.js
+++ app/components/image/image-controller.js
@@ -1,5 +1,6 @@
-//Your ImageService is a global class what can you do here to instantiate it?
-
+//Your ImageService is a global class what can you do here to instantiate it? done(i think)
+import ImageService from "./image-service.js"
+let imageService = new ImageService()
 
 export default class ImageController {
 
--- app/main.js
+++ app/main.js
@@ -1,12 +1,18 @@
 import WeatherController from "./components/weather/weather-controller.js";
-
-// HEY WHERE ARE ALL OF YOUR CONTROLLERS??
+import ImageController from "./components/image/image-controller.js";
+import QuoteController from "./components/quote/quote-controller.js"
+import TodoController from "./components/todo/todo-controller.js"
+// HEY WHERE ARE ALL OF YOUR CONTROLLERS?? done(i think)
 class app {
   constructor() {
     this.controllers = {
-      weatherController: new WeatherController()
+      weatherController: new WeatherController(),
+      imageController: new ImageController(),
+      quoteController: new QuoteController(),
+      todoController: new TodoController()
     }
   }
 }
+
 
 window.app = new app()  
