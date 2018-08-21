4e802b91bfb15379c6592d438bdef054361aa330
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-08-21 15:43:15 -0600

    updated fahrenheit fix

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -4,6 +4,7 @@ var weatherService = new WeatherService()
 let ws = new WeatherService
 
 function draw(ws) {
+	let Fahrenheit = (ws.main.temp * (9 / 5) - 459.67).toFixed(2)
 
 	//trying to get weather
 	//i'm guessing similar to cars / homes arrays
@@ -11,7 +12,7 @@ function draw(ws) {
 	//returning the wrong values...
 	console.log(weatherService.getWeather)
 	//need to get temp out of this not whole weather object
-	var template3 = `<p>weather: ${ws.main.temp}</p>
+	var template3 = `<p>weather: ${Fahrenheit} degrees F</p>
 									`
 
 	document.getElementById("weather").innerHTML = template3
